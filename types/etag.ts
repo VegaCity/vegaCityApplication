7493f4aafdca1ff892ece7c0a2342a55f96f7949
khang29052002 <khangnh2905@gmail.com>
2024-10-06 12:20:51 +0700

    fix to deploy

--- types/etag.ts
+++ types/etag.ts
@@ -1,15 +1,62 @@
+// etag.ts
+
+export interface ETagType {
+  id: string;
+  name: string;
+  marketZoneId: string;
+  imageUrl: string;
+  bonusRate: number;
+  deflag: boolean;
+  amount: number;
+  walletTypeId: string;
+}
+
+export interface MarketZone {
+  id: string;
+  name: string;
+  location: string;
+  imageUrl: string;
+  description: string;
+  address: string;
+  phoneNumber: string;
+  email: string;
+  shortName: string;
+  deflag: boolean;
+  crDate: string;
+  upsDate: string;
+}
+
+export interface Wallet {
+  id: string;
+  walletTypeId: string;
+  crDate: string;
+  upsDate: string;
+  balance: number;
+  balanceHistory: number;
+  deflag: boolean;
+}
+
 export interface ETag {
-    id: string;
-    etagCode: string;
-    fullname: string;
-    phoneNumber: string;
-    cccd: string;
-    imageUrl: string;
-    gender: number;
-    birthday: string;
-    qrCode: string;
-    deflag: boolean;
-    startDate: string;
-    endDate: string;
-    status: number;
-  }
+  id: string;
+  etagCode: string;
+  fullName: string;
+  phoneNumber: string;
+  cccd: string;
+  imageUrl: string;
+  gender: number;
+  birthday: string | null;
+  qrcode: string;
+  deflag: boolean;
+  startDate: string;
+  endDate: string;
+  status: number;
+  crDate: string;
+  upsDate: string;
+  etagTypeId: string;
+  marketZoneId: string;
+  walletId: string;
+  isVerifyPhone: boolean;
+  etagType: ETagType;
+  marketZone: MarketZone;
+  wallet: Wallet;
+}
