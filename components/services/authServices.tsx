36dff296460166cc1f2f89fdee8cf8e136c9aa71
Eric Lee <lethaikhoa0109@gmail.com>
2024-10-14 15:48:56 +0700

    test branch

--- components/services/authServices.tsx
+++ components/services/authServices.tsx
@@ -13,5 +13,6 @@ export const AuthServices = {
     localStorage.removeItem("accessToken");
     localStorage.removeItem("userId");
     localStorage.removeItem("userEmail");
+    localStorage.removeItem("userEmail");
   },
 };
