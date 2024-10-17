b6c71c7d0f3fc00cbb617844ab28bbb0c8bb6f09
Eric Lee <lethaikhoa0109@gmail.com>
2024-10-17 21:45:04 +0700

    config firebase in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
+// next.config.js
+
+const { initializeApp } = require("firebase/app");
+const { getAnalytics } = require("firebase/analytics");
+const { getStorage } = require("firebase/storage");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCM9tvTPe6Lyc8KEBGNMB0cj0q_xlh4t6U",
+  authDomain: "vegacity-utility-card.firebaseapp.com",
+  projectId: "vegacity-utility-card",
+  storageBucket: "vegacity-utility-card.appspot.com",
+  messagingSenderId: "7350286169",
+  appId: "1:7350286169:web:72c2d502a4bc15427afe48",
+  measurementId: "G-LVH7NMV7KX",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Note: Analytics should only be initialized on the client side
+// const analytics = getAnalytics(app)
+const storage = getStorage(app);
+
 module.exports = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
@@ -16,5 +38,13 @@ module.exports = {
         destination: "/",
       },
     ];
+  },
+  // Make Firebase app and storage available to your Next.js app
+  serverRuntimeConfig: {
+    firebaseApp: app,
+    firebaseStorage: storage,
+  },
+  publicRuntimeConfig: {
+    firebaseConfig, // This makes your Firebase config available on the client side
   },
 };
