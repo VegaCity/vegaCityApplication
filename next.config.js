1717886b967f91f52d71bf881a510b8d5290fba9
Eric Lee <lethaikhoa0109@gmail.com>
2024-10-17 16:38:00 +0700

    config

--- next.config.js
+++ next.config.js
@@ -2,32 +2,28 @@ module.exports = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
       },
       {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
       },
     ],
   },
   async redirects() {
     return [
       {
-        source: '/user/order-status',
+        source: "/user/order-status",
         missing: [
           {
-            type: 'query',
-            key: 'status',
+            type: "query",
+            key: "status",
           },
         ],
         permanent: false,
-        destination: '/',
+        destination: "/",
       },
     ];
   },
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> parent of 0893aec (config firebase store)
