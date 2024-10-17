4705e5311c367fb024fcc125f284a7a69099bbdc
Eric Lee <lethaikhoa0109@gmail.com>
2024-10-17 15:54:30 +0700

    Revert "Edit Package, upload Image but still not get url and put into data"
    This reverts commit 2b5515ef66bdabb453c14eeb1637fa2fdaecbb2a.

--- types/etagtype.ts
+++ types/etagtype.ts
@@ -1,25 +1,22 @@
-export interface EtagType {    
+export interface EtagType {
   id: string;
-  name: string;                       
+  name: string;
   imageUrl?: string;
-  bonusRate: number;                      
-  amount: number;              
+  bonusRate: number;
+  amount: number;
 }
 
 export interface EtagTypePost {
-  name: string;                       
+  name: string;
   imageUrl?: string;
-  bonusRate: number;                      
-  amount: number;  
+  bonusRate: number;
+  amount: number;
   walletTypeId: string;
 }
 
-export interface EtagTypePatch {    
-  name: string;                       
+export interface EtagTypePatch {
+  name: string;
   imageUrl?: string;
-  bonusRate: number;                      
-  amount: number;              
+  bonusRate: number;
+  amount: number;
 }
-
-
-  
