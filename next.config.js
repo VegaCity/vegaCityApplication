3f8b616b2ec19e5a1807445e5ae09fa0d5de5a63
Eric Lee <lethaikhoa0109@gmail.com>
2024-10-18 12:21:30 +0700

    fix config firebase and something to deploy

--- next.config.js
+++ next.config.js
@@ -39,12 +39,13 @@ module.exports = {
       },
     ];
   },
-  // Make Firebase app and storage available to your Next.js app
-  serverRuntimeConfig: {
-    firebaseApp: app,
-    firebaseStorage: storage,
-  },
-  publicRuntimeConfig: {
-    firebaseConfig, // This makes your Firebase config available on the client side
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
   },
 };
