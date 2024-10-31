e1e2e7fc59d15da9e7cf5e40c78081f54918bb02
khang29052002 <khangnh2905@gmail.com>
2024-10-31 17:26:45 +0700

    update profile

--- types/user.ts
+++ types/user.ts
@@ -7,11 +7,28 @@ export interface Users {
   crDate: string;
   upsDate: string;
   gender: number;
-  cccd: string;
+  cccdPassport: string;
   imageUrl: string | null;
   email: string;
   roleId: string;
   description: string;
   address: string;
+  status: number;
+}
+
+export interface UserProfileFormData {
+  fullName: string;
+  address: string;
+  description: string | null;
+  phoneNumber: string;
+  birthday: string | null;
+  gender: number;
+  cccdPassport: string;
+  imageUrl: string | null;
+}
+
+export interface ApiResponse<T> {
+  data: T;
+  message?: string;
   status: number;
 }
